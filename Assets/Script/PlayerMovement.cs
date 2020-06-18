77b98ea087e353cc08325916e7f6e28418c759de
Randoom <m.bachich@etudiant.ecolemelies.fr>
2020-06-18 17:20:54 +0200

    petite maj

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -7,19 +7,35 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
 
+    [SerializeField] private float Speed;
+    [SerializeField] private float MaxSpeed;
+
     private Inputs inputs;
+    private Vector2 direction;
+    public Rigidbody2D MyRigidbody;
+
+    
 
     private void OnEnable()
     {
         inputs = new Inputs();
         inputs.Enable();
         inputs.Player.Move.performed += OnMovePerformed;
+        inputs.Player.Move.canceled += OnMoveCanceled;
     }
 
     private void OnMovePerformed(InputAction.CallbackContext obj)
     {
-        Debug.Log("coucou");
+        direction = obj.ReadValue<Vector2>();
+       
     }
+
+    private void OnMoveCanceled(InputAction.CallbackContext obj)
+    {
+        direction = Vector2.zero;
+        MyRigidbody.velocity = Vector2.zero;
+    }
+
 
     // Start is called before the first frame update
     void Start()
@@ -27,9 +43,23 @@ public class PlayerMovement : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MyRigidbody = GetComponent<Rigidbody2D>();
+        //MyRigidbody.MovePosition(direction);
+        //MyRigidbody.velocity = direction;
+
+        if (MyRigidbody.velocity.sqrMagnitude < MaxSpeed)
+        {
+            MyRigidbody.AddForce(direction * Speed);
+        }
+
+     
     }
 }
