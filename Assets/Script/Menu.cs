d9139de92ebeee1beb546b7932b884187b682d32
Randoom <m.bachich@etudiant.ecolemelies.fr>
2020-06-20 23:47:17 +0200

    Ajout rapide mais non fonctionnel

---
+++ Assets/Script/Menu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public void playBouton()
+    {
+        SceneManager.LoadScene("SampleScene");//Charge la scène du jeu
+    }
+
+}
